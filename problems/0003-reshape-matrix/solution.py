079e8f24930eaf7bc48c5b14f5613a1b84d58d7d
Hexatox <128434430+Hexatox@users.noreply.github.com>
2026-09-25 15:49:11 +0000

    Update: Reshape Matrix

--- problems/0003-reshape-matrix/solution.py
+++ problems/0003-reshape-matrix/solution.py
@@ -4,13 +4,10 @@ def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list
 	#Write your code here and return a python list after reshaping by using numpy's tolist() method
 	
 
-	n  = len(a)
-	m= len(a[0])
-
-
-	if n*m != new_shape[0]*new_shape[1]:
-		return []
 	arr = np.array(a)
+
+	if arr.size != new_shape[0] * new_shape[1]:
+        return []
 
 	reshaped_matrix = arr.reshape(new_shape)
 	
